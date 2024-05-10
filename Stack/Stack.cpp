93e4cb9a9114a4fcdd4a4a6e24c6687c74fe13e7
ImronSatrioPurnomo <satriogames2223@gmail.com>
2024-05-10 10:45:31 +0700

    add integer push inside class stack

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -19,5 +19,14 @@ public:
     Stack() {
         top = NULL;
     }
+    int push(int value) {
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "push Value : " << value << endl;
+        return value;
+    }
 
-}
+    
+};
