2e57ba0a7484b603d7f26c88d03d33dc3b32def5
ImronSatrioPurnomo <satriogames2223@gmail.com>
2024-05-10 10:52:28 +0700

    add cout and current inside while

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -43,8 +43,10 @@ public:
         else {
             Node* current = top;
             while (current != NULL) {
-
+                cout << current->data << " " << endl;
+                current = current->next;
             }
+            cout << endl;
         }
     }
 
